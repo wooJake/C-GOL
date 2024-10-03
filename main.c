b14297bfb372683fabb676638a532b525553a758
wooJake <167344799+wooJake@users.noreply.github.com>
2024-10-03 15:40:03 -0700

    Create main.c
    Creating Conways game of life in C

---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    
+    srand(time(NULL));
+    
+    int boardWidth = 64;
+    int boardHeight = 64;
+    
+    int board1[boardWidth][boardHeight];
+    int board2[boardWidth][boardHeight];
+    
+    int neighbors[boardWidth][boardHeight];
+    
+    int loops = 0;
+    
+    //Setting up board
+    for(int i = 0; i < boardWidth; ++i){
+        for(int j = 0; j < boardHeight; ++j){
+            
+            board1[i][j] = rand() % 2;
+            board2[i][j] = board1[i][j];
+            
+            printf("%d", board1[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //Looping
+    while(loops <= 256){
+        
+        //Copying
+        for(int i = 0; i < boardWidth; ++i){
+            for(int j = 0; j < boardHeight; ++j){
+            
+                board1[i][j] = board2[i][j];
+                neighbors[i][j] = 0;
+            }
+            printf("-");
+        }
+        
+        printf("\n");
+        
+        //Interacting
+        for(int i = 0; i < boardWidth; ++i){
+            for(int j = 0; j < boardHeight; ++j){
+                
+                if(board1[i + 1][j + 0] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i + 1][j - 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i + 0][j - 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i - 1][j - 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i - 1][j + 0] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i - 1][j + 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i + 0][j + 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                if(board1[i + 1][j + 1] == 1){
+                    
+                    ++neighbors[i][j];
+                }
+                    
+                if(board1[i][j] == 1 && (neighbors[i][j] == 2 || neighbors[i][j] == 3)){
+                    
+                    board2[i][j] = 1;
+                }
+                else if(board1[i][j] == 0 && neighbors[i][j] == 3){
+                    
+                    board2[i][j] = 1;
+                }
+                else{
+                    
+                    board2[i][j] = 0;
+                }
+                
+                printf("%d", board1[i][j]);
+            }
+            printf("\n");
+        }
+        
+        ++loops;
+    }
+
+    return 0;
+}
