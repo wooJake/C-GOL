647cc9eb036804be18eef957843d4615e1176587
wooJake <167344799+wooJake@users.noreply.github.com>
2024-10-07 11:27:15 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,39 +4,39 @@
 
 int main(){
     
-    srand(time(NULL));
+srand(time(NULL));
+
+int boardWidth = 64;
+int boardHeight = 64;
+
+int board1[boardWidth][boardHeight];
+int board2[boardWidth][boardHeight];
+
+int neighbors[boardWidth][boardHeight];
+
+int loops = 0;
     
-    int boardWidth = 64;
-    int boardHeight = 64;
+//Setting up board
+for(int i = 0; i < boardWidth; ++i){
+for(int j = 0; j < boardHeight; ++j){
     
-    int board1[boardWidth][boardHeight];
-    int board2[boardWidth][boardHeight];
+    board1[i][j] = rand() % 2;
+    board2[i][j] = board1[i][j];
     
-    int neighbors[boardWidth][boardHeight];
+    printf("%d", board1[i][j]);
+}
+printf("\n");
+}
     
-    int loops = 0;
-    
-    //Setting up board
-    for(int i = 0; i < boardWidth; ++i){
-        for(int j = 0; j < boardHeight; ++j){
-            
-            board1[i][j] = rand() % 2;
-            board2[i][j] = board1[i][j];
-            
-            printf("%d", board1[i][j]);
-        }
-        printf("\n");
-    }
-    
-    //Looping
-    while(loops <= 1000){
+//Looping
+while(loops <= 1000){
         
-        //Copying
-        for(int i = 0; i < boardWidth; ++i){
-            for(int j = 0; j < boardHeight; ++j){
-
+	//Copying
+	for(int i = 0; i < boardWidth; ++i){
+	    for(int j = 0; j < boardHeight; ++j){
+	
 		if(board2[i][j] == 1){
-
+	
 			++neighbors[i + 1][j + 0];
 			++neighbors[i + 1][j - 1];
 			++neighbors[i + 0][j - 1];
@@ -46,44 +46,46 @@ int main(){
 			++neighbors[i + 0][j + 1];
 			++neighbors[i + 1][j + 1];
 		}
-            
-                board1[i][j] = board2[i][j];
-            }
-            printf("-");
-        }
+	    
+		board1[i][j] = board2[i][j];
+	    }
+	    printf("-");
+	}
         
-        printf("\n");
-        
-        //Interacting
-        for(int i = 0; i < boardWidth; ++i){
-            for(int j = 0; j < boardHeight; ++j){
-                    
-                if(board1[i][j] == 1){
-                    
-                    printf("O");
-                    
-                    if(neighbors[i][j] != 2 && neighbors[i][j] != 3){
-                        
-                        board2[i][j] = 0;
-                    }
-                }
-                else if(board1[i][j] == 0){
-                    
-                    printf(" ");
-                    
-                    if(neighbors[i][j] == 3){
-                        
-                        board2[i][j] = 1;
-                    }
-                }
-
+	printf("\n");
+	
+	//Interacting
+	for(int i = 0; i < boardWidth; ++i){
+	    for(int j = 0; j < boardHeight; ++j){
+		    
+		if(board1[i][j] == 1){
+		    
+		    printf("O");
+		    
+		    if(neighbors[i][j] != 2 && neighbors[i][j] != 3){
+			
+			board2[i][j] = 0;
+		    }
+	
+			neighbors[i][j] = 0;
+			continue;
+		}
+		    
+		printf(" ");
+		
+		if(neighbors[i][j] == 3){
+		
+			board2[i][j] = 1;
+		}
+		
 		neighbors[i][j] = 0;
-            }
-            printf("\n");
-        }
-        
-        ++loops;
-    }
+		continue;
+	    }
+	    printf("\n");
+	}
+	
+	++loops;
+}
 
     return 0;
 }
