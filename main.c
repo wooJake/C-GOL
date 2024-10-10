dda1e2d859ade2c68c4bb6131a8bf81469b2f553
wooJake <167344799+wooJake@users.noreply.github.com>
2024-10-10 16:11:53 -0700

    Update main.c
    Commenting

--- main.c
+++ main.c
@@ -3,40 +3,40 @@
 #include <time.h>
 #include <unistd.h>
 
-int width = 64;
+int width = 144;
 int height = 64;
 
 int dir[8][2] = {{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1}};
 
-int bornSurvive[2][9] = {{0,0,0,1,0,0,0,0,0}, {0,0,1,1,0,0,0,0,0}};
-int generations = 2;
+int bornSurvive[2][9] = {{0,0,1,0,0,0,0,0,0}, {0,0,0,1,1,1,0,0,0}};
+int generations = 4;
 
 int main(){
     
 	srand(time(NULL));
 
-	int board1[width][height];
-	int board2[width][height];
+	int board1[height][width];
+	int board2[height][width];
 
-	int neighbors[width][height];
+	int neighbors[height][width];
 
 	int loops = 0;
 		
 	//Setting up board
-	for(int i = 0; i < width; ++i){
-		for(int j = 0; j < height; ++j){
+	for(int i = 0; i < height; ++i){
+		for(int j = 0; j < width; ++j){
 			
 			board1[i][j] = rand() % generations;
 			board2[i][j] = board1[i][j];
 		}
 	}
-    
+
 	//Looping
 	while(loops <= 1024){
 			
 		//Copying
-		for(int i = 0; i < width; ++i){
-			for(int j = 0; j < height; ++j){
+		for(int i = 0; i < height; ++i){
+			for(int j = 0; j < width; ++j){
 
 				for(int k = 0; board2[i][j] == 1 && k < 8; ++k){
 
@@ -49,8 +49,8 @@ int main(){
 		system("clear");
 		
 		//Interacting
-		for(int i = 0; i < width; ++i){
-			for(int j = 0; j < height; ++j){
+		for(int i = 0; i < height; ++i){
+			for(int j = 0; j < width; ++j){
 				
 				board2[i][j] = 0;
 
@@ -62,6 +62,7 @@ int main(){
 				//Performing rules
 				for(int k = 0; k < 9; ++k){
 					
+					//Born / survive check.
 					if((bornSurvive[0][k] && board1[i][j] == 0 || 
 						bornSurvive[1][k] && board1[i][j] == 1) && 
 						neighbors[i][j] == k){
@@ -73,6 +74,7 @@ int main(){
 				//Making Dead state blank
 				if(board2[i][j] == 0){
 
+					//If Dead print nothing.
 					printf(" ");
 				}
 				else{
@@ -81,6 +83,7 @@ int main(){
 					printf("%d", board2[i][j]);
 				}
 				
+				//Setting neighbors to 0.
 				neighbors[i][j] = 0;
 			}
 			printf("\n");
